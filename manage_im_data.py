e7801cb1d8793d6ccd45deee1945f5d1ad1ec392
Alex Kuefler <kuefleralex@gmail.com>
2016-06-03 22:50:03 -0700

    Added CIFAR loader. Utilities for creating masks using k-means.

--- manage_im_data.py
+++ manage_im_data.py
@@ -13,6 +13,8 @@ import itertools
 import copy
 import random
 
+import cv2
+
 #from PIL import Image
 from matplotlib import pyplot as plt
 
@@ -45,8 +47,8 @@ def prep_image(im, lw= 128):
     #im = im - MEAN_VALUES
     return rawim, (im[np.newaxis]).astype('float32')
 
-def load_CIFAR():
-    fo = open('./data/CIFAR/cifar-10-batches-py/data_batch_1', 'rb')
+def load_CIFAR(batch_num= 1):
+    fo = open('./data/CIFAR/cifar-10-batches-py/data_batch_'+str(batch_num)+'', 'rb')
     dic = cPickle.load(fo)
     fo.close()
 
@@ -89,9 +91,15 @@ class TT_object(object):
                 self.ims[key] = root+path+angle+im
                 #self.ims[key] = ndimage.imread(root+path+angle+im)
             
-    def retrieve_img(self, degree):
-        im1 = ndimage.imread(self.ims[degree])
-        im2 = np.array(im1)
+    def retrieve_img(self, degree, use_seg= True):
+        if not use_seg:
+            im1 = ndimage.imread(self.ims[degree])
+            im2 = np.array(im1)
+        else:
+            f = self.ims[degree].replace('Bottom','Masks')
+            path = ''.join(f.split('.')[:-1])
+            im2 = np.load('.'+path+'.npz')['arr_0']
+            
         im, cnn_im = prep_image(im2, lw= 2 ** self.scale)
         #im = sp.misc.imresize(im, size= (downsample,downsample))
         return im
@@ -108,9 +116,42 @@ class TT_object(object):
         
         return x1, x2, y
     
+class CIFAR_Data_Loader(object):
+    def __init__(self, max_data= 10000):
+        if max_data is None:
+            self.X_train = load_CIFAR(batch_num= 1)
+            self.X_val = load_CIFAR(batch_num= 2)
+            
+        else:
+            self.X_train = load_CIFAR(batch_num= 1)[:max_data]
+            self.X_val = load_CIFAR(batch_num= 2)[:max_data]
+        
+    def iterate_minibatches(self, batch_size= 10, mode= 'train', shuffle= True):
+
+        if mode == "train":
+            X = self.X_train
+        else:
+            X = self.X_val
+        
+        n = len(X)
+        remainder= n - (batch_size * (n / batch_size))        
+
+        if shuffle:
+            p = np.random.permutation(len(X))
+            X = X[p]
+            
+        minibatches = np.split(np.array(X), (len(X))/batch_size)
+            
+        return minibatches
+    
+    def minibatch_2_Xy(self, minibatch):
+        return minibatch
+        
+        
+    
 class Rot_Data_Loader(object):
     def __init__(self, objects= [('batch1', 'Bannanas'), ('batch1', 'Base')],\
-                 interval = 2, scale= 7):
+                 interval = 2, scale= 7, max_data= None):
         self.objects= [TT_object(path= folder+'/'+item+'/', scale= scale)\
                        for folder, item in objects]
         
@@ -119,10 +160,18 @@ class Rot_Data_Loader(object):
         for obj in self.objects:
             combos= list(itertools.combinations(obj.ims.keys(), 2))
             for combo in combos:
+                if abs(combo[0] - combo[1]) < 10:
+                    #print "skipped!"
+                    continue
+                
                 data.append((obj,)+combo)
                 
         self.train_data = [datapoint for i, datapoint in enumerate(data) if i % interval != 0]
         self.val_data = [datapoint for i, datapoint in enumerate(data) if i % interval == 0]
+        
+        if max_data is not None:
+            self.train_data=self.train_data[0:max_data]
+            self.val_data = self.val_data[0:max_data]
         
     def minibatch_2_Xy(self, minibatch):
         X1= []
@@ -142,7 +191,6 @@ class Rot_Data_Loader(object):
     
     def iterate_minibatches(self, batch_size= 10, mode= 'train', shuffle= True):
 
-        
         if mode == "train":
             d = copy.copy(self.train_data)
         else:
@@ -154,14 +202,71 @@ class Rot_Data_Loader(object):
         if shuffle:
             random.shuffle(d)
             
-        d = d[0:-remainder]
+        if remainder != 0:
+            d = d[0:-remainder]
             
         minibatches = np.split(np.array(d), (len(d))/batch_size)
             
         return minibatches
 
-halt= True
+def segment(filename):
+    img = ndimage.imread(filename)
+    #img = skimage.transform.resize(img, (64, 64))
+    
+    Z = img.reshape((-1,3))
+    
+    # convert to np.float32
+    Z = np.float32(Z)
+    
+    # define criteria, number of clusters(K) and apply kmeans()
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    K = 7
+    ret,label,center=cv2.kmeans(Z,K,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+    
+    # Now convert back into uint8, and make original image
+    center = np.uint8(center)
+    res = center[label.flatten()]
+    res2 = res.reshape((img.shape))
 
+    mask1= (res2[:,:,0] > 110).astype('int32') * (res2[:,:,2] > 105).astype('int32')
+    #mask2 = (res2.argmax(axis= 2) == 1).astype('int')
+    
+    mask = mask1
+    mask= np.dstack((mask, mask, mask))
+    
+    #plt.imshow(img * mask)
+    idx= (mask1 == 1)
+    Y= img.copy()
+    Y[idx] = 0
+    
+    return Y
+
+def create_mask_data(path):
+    print "CREATING MASKS"
+    ldir= sorted(os.listdir(path+'/Bottom'))
+    for f in ldir:
+        if f.split('.')[-1] == 'JPG':
+            if int(f.split('-')[1].split('_')[0]) > 360:
+                continue
+            
+            try:
+                seg = segment(path+'/Bottom/'+f)
+                try:
+                    np.savez(path+'/Masks/'+(''.join(f.split('.')[:-1])), seg)
+                except IOError:
+                    os.mkdir(path+'/Masks')
+                    np.savez(path+'/Masks/'+(''.join(f.split('.')[:-1])), seg)
+                    
+            except ValueError:
+                print "skipped: ", path+'/Bottom/'+f
+         
+    #halt= True
+    
+#for toy in ['TeddyBear']:
+    #print toy
+    #create_mask_data('./data/CALTECH/batch1/'+toy)
+    
+#halt= True
         
 #tt = TT_object()
 #x1, x2, y = tt.retrieve_example(270, 295)
